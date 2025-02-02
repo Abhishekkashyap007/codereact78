49bd52c0898f3d4a30b994e4731a35ebf1bb1ec4
Abhishekkashyap007 <ak555585955@gmail.com>
2025-02-03 00:08:57 +0530

    image src

--- src/About.js
+++ src/About.js
@@ -1,5 +1,20 @@
+import { Fragment } from "react";
+import myimage from './photo/img1.jpg';
+
+const abhi="https://th.bing.com/th/id/OIP.iZSGztm8JFodKPVmqWkc6QHaEK?rs=1&pid=ImgDetMain";
+
 function Abc()
 {
-    return "this is web";
+    return(
+        <Fragment>
+        <h1>this is heading</h1>
+        <h2>this is heading2</h2>
+        <img src={myimage} alt="yes"/>
+        <img src="photo/img1.jpg" alt="yes1"/>
+        <img src={abhi} alt="yes3"/>
+
+        </Fragment>
+        
+    )
 }
 export default Abc;
